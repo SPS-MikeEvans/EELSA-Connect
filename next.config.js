914a6ce5d263cab5186f7131ebc1e66cecaa22b6
M Evans <accounts@summitpsychologyservices.co.uk>
2026-02-02 13:07:39 +0000

    Downgraded pdfjs-distversion 4.0.379 and pdated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
